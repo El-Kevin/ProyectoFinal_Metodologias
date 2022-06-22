24cddd1af25d72f7d290d12e4d47f0fac6a141a5
Anthony Alexander Chamba Elizalde <anthony.chamba@epn.edu.ec>
2022-06-22 12:45:17 -0500

    Iteration 1

--- src/main/java/ec/edu/epn/Main.java
+++ src/main/java/ec/edu/epn/Main.java
@@ -2,20 +2,25 @@ package ec.edu.epn;
 
 import ec.edu.epn.Manejador.ManejadorUsuario;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args)  {
+        Scanner sc = new Scanner(System.in);
         System.out.println("Hola pelotudos");
         System.out.println("Alucination");
         String pathFile = System.getProperty("user.dir") + "/src/main/resources/dataUsuario.json";
         System.out.println(pathFile);
-        ManejadorUsuario manejadorUsuario = new ManejadorUsuario(pathFile);
-
-        try {
-            System.out.println(manejadorUsuario.leerArchivoUsuarios().get(0).getNumeroCedula());
-        } catch (Exception e){
-            e.printStackTrace();
-        }
-
+//        ManejadorUsuario manejadorUsuario = new ManejadorUsuario(pathFile);
+//
+//        try {
+//            System.out.println(manejadorUsuario.leerArchivoUsuarios().get(0).getNumeroCedula());
+//        } catch (Exception e){
+//            e.printStackTrace();
+//        }
+        System.out.println("Ingrese algo");
+        String letter = sc.nextLine();
+        System.out.println(letter);
     }
 
 }
