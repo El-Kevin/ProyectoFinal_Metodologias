a909877c18634a088979cb7b39fd945a55a30bf0
Anthony Alexander Chamba Elizalde <anthony.chamba@epn.edu.ec>
2022-06-22 11:43:31 -0500

    Testing if it really reads json file

--- src/main/java/ec/edu/epn/Main.java
+++ src/main/java/ec/edu/epn/Main.java
@@ -1,9 +1,19 @@
 package ec.edu.epn;
 
+import ec.edu.epn.Manejador.ManejadorUsuario;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args)  {
         System.out.println("Hola pelotudos");
         System.out.println("Alucination");
+        String pathFile = System.getProperty("user.dir") + "/src/main/resources/dataUsuario.json";
+        ManejadorUsuario manejadorUsuario = new ManejadorUsuario(pathFile);
+        try {
+            System.out.println(manejadorUsuario.leerArchivoUsuarios().get(0).getNumeroCedula());
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+
     }
 
 }
