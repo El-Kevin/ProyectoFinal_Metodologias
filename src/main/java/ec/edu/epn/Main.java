2e675958d1b2980edd5813aea8c353da097bb2db
Anthony Alexander Chamba Elizalde <anthony.chamba@epn.edu.ec>
2022-06-22 10:56:38 -0500

    Craar un repositorio para el proyecto con Maven y las clases con sus respectivos metodos(vacios) y atributos

--- src/main/java/ec/edu/epn/Main.java
+++ src/main/java/ec/edu/epn/Main.java
@@ -3,5 +3,7 @@ package ec.edu.epn;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hola pelotudos");
+        System.out.println("Alucination");
     }
+
 }
