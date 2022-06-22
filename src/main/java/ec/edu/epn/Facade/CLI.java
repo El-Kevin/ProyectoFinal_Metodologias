542ac2b7fc2da213c5ad2b29be96f5427ed48652
Anthony Alexander Chamba Elizalde <anthony.chamba@epn.edu.ec>
2022-06-21 22:44:57 -0500

    Craar un repositorio para el proyecto con Maven y las clases con sus respectivos metodos(vacios) y atributos

--- src/main/java/ec/edu/epn/Facade/CLI.java
+++ src/main/java/ec/edu/epn/Facade/CLI.java
@@ -7,18 +7,25 @@ import ec.edu.epn.Usuario.Paciente;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class CLI {
     private boolean claveAutenticada;
     private CitaMedica citaMedica;
     private ManejadorUsuario mu = new ManejadorUsuario(System.getProperty("user.dir") + "/src/main/resources/dataUsuario.json");
 ;
-    public boolean autenticarPacienteMedianteCedula(String numeroCedula, String password) throws FileNotFoundException {
+    public boolean autenticarPacienteMedianteCedula() throws FileNotFoundException {
+        Scanner sc = new Scanner(System.in);
+        String numeroCedula;
+        String password;
+        System.out.println("Ingrese su numero de cedula");
+        numeroCedula = sc.nextLine();
+        System.out.println("Ingrese su contrasenia");
+        password = sc.nextLine();
         ArrayList<Paciente> pacientes = mu.leerArchivoUsuarios();
         for (Paciente p : pacientes
              ) {
-            System.out.println("cedula " + p.getNumeroCedula() + " password " + p.getClave());
-            System.out.println("numeroCedula " + numeroCedula + " password " + password);
+
             if (p.getNumeroCedula().equals(numeroCedula) && p.getClave().equals(password)) {
                 return true;
             }
