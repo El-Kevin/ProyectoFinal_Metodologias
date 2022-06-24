d235760191d40dfcca31917237d1ebc0c166f177
Anthony Alexander Chamba Elizalde <anthony.chamba@epn.edu.ec>
2022-06-24 11:51:34 -0500

    Iteration 1

--- src/main/java/ec/edu/epn/Main.java
+++ src/main/java/ec/edu/epn/Main.java
@@ -2,6 +2,9 @@ package ec.edu.epn;
 
 import ec.edu.epn.Manejador.ManejadorUsuario;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class Main {
@@ -18,10 +21,15 @@ public class Main {
 //        } catch (Exception e) {
 //            e.printStackTrace();
 //        }
-        String nombre;
-        System.out.print("Por favor, dime tu nombre: ");
-        nombre = System.console().readLine();
-        System.out.println("Hola " + nombre + ", ¡bienvenido a Java desde Cero!");
+        try {
+            System.out.print("Enter Username: ");
+            InputStreamReader streamReader = new InputStreamReader(System.in);
+            BufferedReader bufferedReader = new BufferedReader(streamReader);
+            String username = bufferedReader.readLine();
+            System.out.println("Your username is : " + username );
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 }
