1d82d8487b28d502ace19756697422ec7d7ef87c
Anthony Alexander Chamba Elizalde <anthony.chamba@epn.edu.ec>
2022-06-24 10:47:24 -0500

    Iteration 1

--- src/test/java/ec/edu/epn/anthony/AdminTest.java
+++ src/test/java/ec/edu/epn/anthony/AdminTest.java
@@ -7,6 +7,7 @@ import org.mockito.Mockito;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.Scanner;
 
 import static org.junit.Assert.*;
 
@@ -16,6 +17,11 @@ public class AdminTest {
         IiniciarSesion is = Mockito.mock(IiniciarSesion.class);
         Paciente p = new Paciente("1752331700", "Kevin", "Toasa", "Caupicho", "Terapia", "Rammor123");
         Admin a = new Admin();
+        Scanner sc = new Scanner(System.in);
+        String something;
+        System.out.println("Ingrese algo");
+        something = sc.nextLine();
+        System.out.println(something);
         Mockito.when(is.iniciarSesion(a.getId(), a.getContrasenia())).thenReturn(true);
         if (!a.getHaIniciadoSesion()) {
             a.setHaIniciadoSesion(is.iniciarSesion(a.getId(), a.getContrasenia()));
